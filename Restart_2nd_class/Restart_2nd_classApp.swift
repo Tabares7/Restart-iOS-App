11aa01018b325ac2d147d01f8ecfcbe3649f0d47
tabares7 <pedrotabares777@gmail.com>
2023-01-14 12:22:25 -0600

    Initial Commit

---
+++ Restart_2nd_class/Restart_2nd_classApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_2nd_classApp.swift
+//  Restart_2nd_class
+//
+//  Created by Front Desk  on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Restart_2nd_classApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
